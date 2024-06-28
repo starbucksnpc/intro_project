679314f661e5ff7bb23c3319f05597321e7cb48b
Sol Nam <cbkpwr@gmail.com>
2024-06-28 10:57:48 -0500

    fetched api for shorts

--- app/controllers/shorts_controller.rb
+++ app/controllers/shorts_controller.rb
@@ -1,9 +1,5 @@
 class ShortsController < ApplicationController
-    def index
-      @shorts = ShortsService.fetch_shorts  # From Shorts API, get a shorts list
-    rescue StandardError => e
-      flash[:error] = "Error fetching shorts: #{e.message}"
-      @shorts = []  # if error
-    end
+  def index
+    @shorts = Short.all
   end
-  
+end
