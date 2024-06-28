89357b02b987ad801fdeb1009b0ed3ea5cf4bf27
Sol Nam <cbkpwr@gmail.com>
2024-06-28 11:09:49 -0500

    fetched api for villains

--- app/controllers/villains_controller.rb
+++ app/controllers/villains_controller.rb
@@ -1,8 +1,5 @@
 class VillainsController < ApplicationController
-    def index
-      @villains = VillainsService.fetch_villains  # From Villains API, get a villains list
-    rescue StandardError => e
-      flash[:error] = "Error fetching villains: #{e.message}"
-      @villains = []  # if error
-    end
+  def index
+    @villains = Villain.all
   end
+end
