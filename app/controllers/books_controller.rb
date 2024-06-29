1206b72dcbe59609317354d010d7331a29a8a0eb
Sol Nam <cbkpwr@gmail.com>
2024-06-28 22:38:44 -0500

    simple search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,14 @@
 class BooksController < ApplicationController
-
   def index
-    @books = Book.page(params[:page]).per(10)
+    if params[:query].present?
+      query = "%#{params[:query].downcase}%"
+      @books = Book.where('LOWER(title) LIKE ?', query).page(params[:page]).per(10)
+    else
+      @books = Book.page(params[:page]).per(10)
+    end
   end
 
   def show
     @book = Book.find(params[:id])
   end
-  
-  
 end
