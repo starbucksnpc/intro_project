d94144bd254a690845e5e24512f926913c4eed42
Sol Nam <cbkpwr@gmail.com>
2024-07-01 11:27:56 -0500

    fixed gender nil issue and villain's individual pages issue

--- app/controllers/villains_controller.rb
+++ app/controllers/villains_controller.rb
@@ -1,26 +1,35 @@
 class VillainsController < ApplicationController
-  before_action :set_genders, only: [:index]
+  before_action :set_genders, only: [:index, :show]
+  before_action :set_villain, only: [:show]
 
   def index
-    @genders = ['Male', 'Female', 'Other'] # Define the genders for the dropdown
-
     if params[:query].present? && params[:gender].present?
       query = "%#{params[:query].downcase}%"
-      @villains = Villain.where('LOWER(name) LIKE ? AND gender = ?', query, params[:gender])
+      @villains = Villain.where('LOWER(name) LIKE ? AND LOWER(gender) = ?', query, params[:gender].downcase)
                          .distinct.page(params[:page]).per(10)
     elsif params[:query].present?
       query = "%#{params[:query].downcase}%"
       @villains = Villain.where('LOWER(name) LIKE ?', query).distinct.page(params[:page]).per(10)
     elsif params[:gender].present?
-      @villains = Villain.where(gender: params[:gender]).distinct.page(params[:page]).per(10)
+      @villains = Villain.where('LOWER(gender) = ?', params[:gender].downcase).distinct.page(params[:page]).per(10)
     else
       @villains = Villain.distinct.page(params[:page]).per(10)
     end
+  end
+
+  def show
+
   end
 
   private
 
   def set_genders
     @genders = ['male', 'female', 'other']
+  end
+
+  def set_villain
+    @villain = Villain.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to villains_path, alert: "Villain not found."
   end
 end
